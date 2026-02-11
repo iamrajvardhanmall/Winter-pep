b487f9644a254dee1f15b50fe934dfae8cf78a50
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-02-11 12:18:32 +0530

    Describe what you changed

--- 11-02-2026/pattern.py
+++ 11-02-2026/pattern.py
@@ -5,17 +5,17 @@
 #     print("*" * i)
 
 
-for i in range(5, 0, -1):
-    for j in range(1 , i + 1):
-        print(j, end= " ")
-    for j in range(1, i+1):
-        print(j, end=" ")
-    print()
+# for i in range(5, 0, -1):
+#     for j in range(1 , i + 1):
+#         print(j, end= " ")
+#     for j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
     
-for i in range(2, 6):
-    for j in range(1, i+ 1):
-        print(j, end=" ")
-    print()
+# for i in range(2, 6):
+#     for j in range(1, i+ 1):
+#         print(j, end=" ")
+#     print()
 
 
 
@@ -31,3 +31,9 @@ for i in range(2, 6):
 
 
 
+for i in range(1, 6):
+    a = 65
+    for j in range(1, i+1):
+        print(chr(a), end=" ")
+        a += 1
+    print()
