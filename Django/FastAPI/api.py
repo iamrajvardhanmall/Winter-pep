c223028f990fec5f3ff5706ffce060140f721a76
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-02-12 15:49:52 +0530

    http://127.0.0.1:8000/docs

--- Django/FastAPI/api.py
+++ Django/FastAPI/api.py
@@ -3,5 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def LPU():
-    return {"Hello World"}
-print(LPU())
+    return ("Hello World")
+# print(LPU())
+
+@app.get("/about")
+def about():
+    return ("LPU is good place to live")
+print(about())
