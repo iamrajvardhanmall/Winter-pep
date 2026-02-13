e032fd433fa34e47f3f3e4319fed03684a6f0d65
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-02-13 15:45:49 +0530

    FastAPI tags

--- Django/FastAPI/api.py
+++ Django/FastAPI/api.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
-app = FastAPI()
-@app.get("/")
+app = FastAPI(title="LPU")
+
+# @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
+# def LPU():
+#     return ("Hello World")
+
+
+@app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
 def LPU():
-    return ("Hello World")
-# print(LPU())
+    return {
+            "msg": "Hello World"
+    }
 
-@app.get("/about")
-def about():
-    return ("LPU is good place to live")
-print(about())
+
+
+# @app.get("/about")
+# def about():
+#     return ("LPU is good place to live")
