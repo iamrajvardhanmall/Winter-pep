d15fc3338e4628738f3283dbd693001994600fb2
Rajvardhan Mall <144551761+iamrajvardhanmall@users.noreply.github.com>
2026-02-23 16:27:54 +0530

    Refactor FastAPI application and load data from JSON

--- FastAPI/api.py
+++ FastAPI/api.py
@@ -1,33 +1,49 @@
-from fastapi import FastAPI, status
+# from fastapi import FastAPI, status
 
-app = FastAPI(title="LPU")
+# app = FastAPI(title="LPU")
 
-# @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
-# def LPU():
-#     return ("Hello World")
-
-
-
-# @app.get("/about")
-# def about():
-#     return ("LPU is good place to live")
+# # @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
+# # def LPU():
+# #     return ("Hello World")
 
 
-# @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
-# def LPU():
-#     return {
-#             "msg": "Hello World"
-#     }
 
-# @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
-# def indexPostViews(data:str):
-#     return {
-#             "msg": "Hello World"
-#     }
+# # @app.get("/about")
+# # def about():
+# #     return ("LPU is good place to live")
+
+
+# # @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
+# # def LPU():
+# #     return {
+# #             "msg": "Hello World"
+# #     }
+
+# # @app.get("/", tags=['main'], status_code=status.HTTP_404_NOT_FOUND)
+# # def indexPostViews(data:str):
+# #     return {
+# #             "msg": "Hello World"
+# #     }
     
 
-@app.get("/post/{id}")
-def indexView(id:int):
-    return {
-        'id': "id"
-    }
+# @app.get("/post/{id}")
+# def indexView(id:int):
+#     return {
+#         'id': "id"
+#     }
+
+
+
+from fastapi import FastAPI
+import json
+
+app = FastAPI(title="My FastAPI Application")
+@app.get("/", tags=["main"], status_code=200)
+
+def index():
+    data  = load_data()
+    return data
+
+def load_data():
+    with open("student.json","r") as f:
+        return json.load(f)
